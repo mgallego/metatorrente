54488bfbbb22df7d7eedd65d863ee1a001366c65
Moises Gallego <moisesgallego@gmail.com>
2013-05-01 15:31:13 +0200

    Added a new test to the scraper

--- scrapers/test/kat_scraper_test.py
+++ scrapers/test/kat_scraper_test.py
@@ -9,6 +9,9 @@ class TestKatScraper(unittest.TestCase):
     def test_get_torrent(self):
         self.assertTrue(len(self.scraper.get_torrents('ubuntu')) > 0)
 
+    def test_get_no_torrent(self):
+        self.assertTrue(len(self.scraper.get_torrents('thisstringmustnotexistsssssssssaaaaaassss')) == 0)
+
 
 if __name__ == '__main__':
     unittest.main()
