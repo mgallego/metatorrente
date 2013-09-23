73c7c643f31db0013105331ce10343ae3b838a57
Moises Gallego <moisesgallego@gmail.com>
2013-09-23 22:06:01 +0200

    [MTT-1] - Use jsonp instead of json in the api feature

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+import json
+from functools import wraps
+from flask import Flask, render_template, redirect, request, current_app, jsonify
 from flask.ext import restful
 from scrapers.scrap_all import ScrapAll
 
 app = Flask(__name__)
 api = restful.Api(app)
 
+
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f().data) + ')'
+            return current_app.response_class(content, mimetype='application/json')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
+
 class Torrent(restful.Resource):
+    @support_jsonp
     def get(self, description):
         scraper = ScrapAll()
         return scraper.get_torrents(description)
